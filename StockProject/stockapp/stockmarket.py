0a1d3f4a092edbd23e5d82ad58b6eb9a04d327a2
Brandon Christof <brandonchristof@gmail.com>
2021-03-21 17:22:17 -0400

    Added helper file for getting stock data

--- StockProject/stockapp/stockmarket.py
+++ StockProject/stockapp/stockmarket.py
@@ -1,3 +1,33 @@
+'''
+    Helper file to get stock information for a given stock
+    using the yfinance API.
+'''
+
 import yfinance as yf
 
+def getCurrentPrice(stock):
+    today = stock.history(period='1d')
+    return today['Close'][0]
+
+def getVolume(stock):
+    return stock.info["volume"]
+
+def getMarketCap(stock):
+    return stock.info["marketCap"]
+
+def getLogo(stock):
+    return stock.info["logo_url"]
+
+def getName(stock):
+    return stock.info["longName"]
+
+def getStockData(ticker):
+    stock = yf.Ticker(ticker)
+    data = {}
+    data["price"] = getCurrentPrice(stock)
+    data["volume"] = getVolume(stock)
+    data["marketcap"] = getMarketCap(stock)
+    data["logolink"] = getLogo(stock)
+    data["name"] = getName(stock)
+    return data
 
